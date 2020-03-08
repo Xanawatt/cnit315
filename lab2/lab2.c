d8cd7879cff96650185d2a61648b1cb3f5581e84
Xanawatt <Schneidermark2000@gmail.com>
2020-03-08 17:53:17 -0400

    first test

---
+++ lab2/lab2.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<math.h>
+
+#define SIZE = 6
+
+/*
+ * Lab Title: Hot Tub/Pool
+ * Lab Description: Find the size, water capacity, 
+ * 	and cost required to fill a pool and hot tub 
+ * 	using user specified inputs.
+ * Name: Mark Schneider
+ * Email: schne112@purdue.edu
+ * Lab time: 7:30 F
+ */
+ char maze[6][6] = {
+	 {'S','#','#','#','#','#'},
+	 {'.','.','.','.','.','.'},
+	 {'#','.','#','#','#','#'},
+	 {'#','.','#','#','#','#'},
+	 {'.','.','.','#','.','G'},
+	 {'#','#','.','.','.','#'}
+ };
+ 
+	 
+	 
+ 
+ 
+ int mazeGo(int x, int y) {
+	
+	// if position is 'G'
+	if (maze[x][y] == 'G')) {
+		exit;
+	} 
+	 
+	if (x >= 0 && y >= 0 && x < SIZE && y < SIZE && maze[x][y] != '+' && maze[x][y] != '#') {
+		maze[x][y] = '+';
+		
+		// North
+		if (mazeGo(x-1, y)) {
+			return 1;
+		}
+		
+		// South
+		if (mazeGo(x+1, y)) {
+			return 1;
+		}
+		
+		// East
+		if (mazeGo(x, y-1)) {
+			return 1;
+		}
+		
+		if (mazeGo(x, y+1)) {
+			return 1;
+		}
+		return 0;
+	}
+	return 0;
+}
+ 
+ int main() {
+	mazeGo(0,0);
+ }
+ 
